5620b32f818ff3c1538932937b15ff9815c1da17
Ashi Jain <64536205+Ashi-Jain@users.noreply.github.com>
2022-04-10 17:36:34 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
 echo "[theme]
 primaryColor='#477569'
-backgroundColor='#fff700'
-secondaryBackgroundColor='#171777'
-textColor='#000006'
+backgroundColor='#0b1077'
+secondaryBackgroundColor='#ff9916'
+textColor='#fefeff'
 font = 'sans serif'
 [server]
 headless = true
